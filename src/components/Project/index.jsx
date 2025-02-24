8f732ca27739ff6a78a4ab7b42aa8382e7fadf97
acastano0203 <acastano0203@gmail.com>
2025-02-24 11:24:51 -0500

    delete third card of the project

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -18,7 +18,7 @@ const Project = () => {
         Mis Proyectos
       </h2>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-4 max-w-6xl mx-auto ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 max-w-6xl mx-auto ">
         {/* Primera Card */}
         <div className="bg-white rounded-xl border border-gray-200 shadow-md  hover:shadow-xl transition-all duration-300 ease-in-out animate-pulse-subtle max-w-sm h-[300px] flex flex-col justify-between transform perspective-1000 hover:rotate-[10deg] hover:scale-105 my-4  px-4 pb-6">
           <div>
@@ -68,34 +68,6 @@ const Project = () => {
             </div>
             <a
               href="https://www.tazamaestra.com.co/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block bg-[#9aca3c] text-black px-6 py-2 rounded-full hover:bg-opacity-90 transition-all font-poppins"
-            >
-              Demo
-            </a>
-          </div>
-        </div>
-
-        {/* Tercera Card */}
-        <div className="bg-white rounded-xl border border-gray-200 shadow-md  hover:shadow-xl transition-all duration-300 ease-in-out animate-pulse-subtle max-w-sm h-[300px] flex flex-col justify-between transform perspective-1000 hover:rotate-[10deg] hover:scale-105 my-4  px-4 pb-6 bg-red-300">
-          <div>
-            <h3 className="text-[200%] font-bold text-[#000080] mb-4 underline font-poppins">
-              Recetas
-            </h3>
-            <p className="text-[100%] text-gray-600 mb-4 pt-6 font-poppins">
-              Una app que te inspira a cocinar con recetas fáciles y deliciosas,
-              organizadas por categorías y con instrucciones paso a paso.
-            </p>
-          </div>
-          <div className="mt-auto">
-            <div className="flex flex-wrap gap-3 mb-6">
-              <FaReact className="text-[100%] text-[#61DAFB]" />
-              <SiSass className="text-[100%] text-[#CC6699]" />
-              <SiTypescript className="text-[100%] text-[#3178C6]" />
-            </div>
-            <a
-              href="https://prueba-tecnica-frontend2025.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block bg-[#9aca3c] text-black px-6 py-2 rounded-full hover:bg-opacity-90 transition-all font-poppins"
